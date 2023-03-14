73e2fadf19bf0ec8998462c863fc9104802db8cc
My Name <my-name@chromium.org>
2023-03-14 09:14:31 -0700

    Reset button for resetting the clock  added

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "preact/hooks";
 import { PeriodicTable } from "./components/periodic-table";
-
+import React, { useSate } from "react";
 export function App() {
   const [startTime, setStartTime] = useState(0);
   const [currTime, setCurrTime] = useState(0);
@@ -10,11 +10,20 @@ export function App() {
       setCurrTime(new Date().getTime());
     }, 1000);
   }, []);
+
+  const resetTime = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    const button: HTMLButtonElement = event.currentTarget;
+    setStartTime(new Date().getTime());
+    setCurrTime(new Date().getTime());
+  };
   return (
     <>
       <h1>
         Hewitt Learning Chemistry games!{" "}
-        {Math.round((currTime - startTime) / 1000)}
+        {Math.round((currTime - startTime) / 1000)}{" "}
+        <button onClick={resetTime}>Reset Clock</button>
       </h1>
       <PeriodicTable />
     </>
