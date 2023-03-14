f32e8da96cca24bb92ba388e3271d98be52bd8f5
My Name <my-name@chromium.org>
2023-03-13 23:21:54 -0700

    Added a count-up stopwatch on the page.

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,21 @@
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 import { PeriodicTable } from "./components/periodic-table";
 
 export function App() {
+  const [startTime, setStartTime] = useState(0);
+  const [currTime, setCurrTime] = useState(0);
+  useEffect(() => {
+    setStartTime(new Date().getTime());
+    setInterval(() => {
+      setCurrTime(new Date().getTime());
+    }, 1000);
+  }, []);
   return (
     <>
-      <h1>Hewitt Learning Chemistry games!</h1>
+      <h1>
+        Hewitt Learning Chemistry games!{" "}
+        {Math.round((currTime - startTime) / 1000)}
+      </h1>
       <PeriodicTable />
     </>
   );
