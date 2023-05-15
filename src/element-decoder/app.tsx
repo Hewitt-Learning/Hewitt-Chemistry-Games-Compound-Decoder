4455d400b16024953599827d3848703d593ffb3a
AaronTheBaron15 <aaborjas017@gmail.com>
2023-05-15 16:51:30 -0700

    created a 'popup' instead of overriding periodic-table with level options

--- src/element-decoder/app.tsx
+++ src/element-decoder/app.tsx
@@ -1,11 +1,69 @@
 import { Level, PeriodicTable } from "./components/periodic-table";
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 import Button from "./components/button";
 import "./app.css";
 
 export function App() {
   const [selectedLevel, setSelectedLevel] = useState<Level | null>(null);
   const [showIntro, setShowIntro] = useState(true);
+  const [showLevel, setShowLevel] = useState(false);
+
+  // when the selectedLevel state changes, useEffect updates and
+  // listens for an escape key press when the difficulty options are displays,
+  // but only after the difficulty is initially set
+  // (e.g. you have to select difficulty the first time you play, but not after)
+  useEffect(() => {
+    const keyListener = (e: KeyboardEvent) => {
+      if (selectedLevel === null) {
+        return;
+      }
+      if (e.key === "Escape") {
+        setShowLevel(false);
+      }
+    };
+    window.addEventListener("keydown", keyListener);
+    //cleanup function, called whenever the component is unmounted
+    return () => {
+      window.removeEventListener("keydown", keyListener);
+    };
+  }, [selectedLevel]);
+
+  //handles what happens when the user asks to change difficulty -
+  // either the user can change difficulty, or they can be exit the change difficulty screen
+  // when the difficulty has previously been selected
+  const difficultyChange = () => {
+    return (
+      <div
+        class="difficulty-chooser-wrapper"
+        onClick={(e) => {
+          // if the click did not originate on the background
+          // (e.g. the difficulty selector box), ignore the click
+          if (e.target !== e.currentTarget) {
+            return;
+          }
+
+          if (selectedLevel === null) {
+            return;
+          }
+          setShowLevel(false);
+        }}
+      >
+        <div class="difficulty-chooser">
+          <div class="difficulty-chooser-text">Select Difficulty</div>
+          
+          <Button onClick={() => handleLevelChange(Level.Beginner)}>
+            Beginner
+          </Button>
+          <Button onClick={() => handleLevelChange(Level.Intermediate)}>
+            Intermediate
+          </Button>
+          <Button onClick={() => handleLevelChange(Level.Advanced)}>
+            Advanced
+          </Button>
+        </div>
+      </div>
+    );
+  };
 
   const playIntroSound = () => {
     const audio = new Audio("/audio/KH3V1.wav");
@@ -16,11 +74,13 @@ export function App() {
   const handleStartButtonClick = () => {
     playIntroSound();
     setShowIntro(false);
+    setShowLevel(true);
   };
 
   const handleLevelChange = (level: Level) => {
     playIntroSound();
     setSelectedLevel(level);
+    setShowLevel(false);
   };
 
   return (
@@ -36,30 +96,26 @@ export function App() {
         </div>
       )}
 
-      {!showIntro &&
-        (selectedLevel === null ? (
-          <div class="difficulty-chooser">
-            <Button onClick={() => handleLevelChange(Level.Beginner)}>
-              Beginner
-            </Button>
-            <Button onClick={() => handleLevelChange(Level.Intermediate)}>
-              Intermediate
-            </Button>
-            <Button onClick={() => handleLevelChange(Level.Advanced)}>
-              Advanced
-            </Button>
-          </div>
-        ) : (
-          <>
-            <PeriodicTable
-              level={selectedLevel}
-              setSelectedLevel={setSelectedLevel}
-            />
-            <Button onClick={() => setSelectedLevel(null)}>
-              Change Difficulty
-            </Button>
-          </>
-        ))}
+      {!showIntro && (
+        <>
+          {showLevel && difficultyChange()}
+          {selectedLevel !== null && (
+            <>
+              <PeriodicTable
+                level={selectedLevel}
+                setSelectedLevel={setSelectedLevel}
+              />
+              <Button
+                onClick={() => {
+                  setShowLevel(true);
+                }}
+              >
+                Change Difficulty
+              </Button>
+            </>
+          )}
+        </>
+      )}
     </>
   );
 }
