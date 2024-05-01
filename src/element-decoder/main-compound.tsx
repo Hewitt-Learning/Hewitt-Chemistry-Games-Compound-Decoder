580ac80437107100a7fc57af14271978c9fd0f3f
bgibson25 <bgibson25@my.whitworth.edu>
2024-04-30 22:38:32 -0700

    separate games working

---
+++ src/element-decoder/main-compound.tsx
@@ -0,0 +1,14 @@
+import { render } from "preact";
+import { subscribeToTheme } from "../theme";
+import { App } from "./app";
+import "./index.css";
+
+render(<App difficulty="Compound" />, document.getElementById("app") as HTMLElement);
+
+subscribeToTheme((theme) => {
+  for (const className of document.body.classList.values()) {
+    if (className.startsWith("theme-"))
+      document.body.classList.remove(className);
+  }
+  document.body.classList.add(`theme-${theme}`);
+});
